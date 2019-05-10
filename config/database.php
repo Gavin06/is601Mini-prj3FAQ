3be54e8b16167b38b4dfc86bec7754523c7fcdda
gdp9 <gdp9@njit.edu>
2019-05-09 22:12:23 -0400

    updates

--- config/database.php
+++ config/database.php
@@ -1,5 +1,7 @@
 <?php
 
+$heroku_db_url = parse_url(env('DATABASE_URL', "postgres://forge:forge@localhost:5432/forge"));
+
 use Illuminate\Support\Str;
 
 return [
@@ -85,6 +87,17 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
+        ],
+
+        'pg-heroku' => [
+            'driver' => 'pgsql',
+            'host' => $heroku_db_url['host'],
+            'database' => substr($heroku_db_url['path'], 1),
+            'username' => $heroku_db_url['user'],
+            'password' => $heroku_db_url['pass'],
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
         ],
 
     ],
